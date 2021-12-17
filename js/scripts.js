bd63451461ecc4e443148256b8340ec1d10101bc
sjd58 <dicksonsj0798@gmail.com>
2021-12-17 12:41:52 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-alert('Hello World!');
-let favoriteFood = 'Chocolate';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Articuno',
+        type: ['flying', 'ice'],
+        height: 9,
+        pokedexNumber: '#145',
+    },
+    {
+        name: 'Zapdos',
+        type: ['flying', 'electric'],
+        height: 10,
+        pokedexNumber: '#145',
+    },
+    {
+        name: 'Moltres',
+        type: ['flying', 'fire'],
+        height: 12,
+        pokedexNumber: '#146',
+    },
+    {
+        name: 'Lugia',
+        type: ['flying', 'psychic'],
+        height: 14,
+        pokedexNumber: '#249',
+    },
+    {
+        name: 'Ho-oh',
+        type: ['flying', 'fire'],
+        height: 13,
+        pokedexNumber: '#250',
+    },
+];
