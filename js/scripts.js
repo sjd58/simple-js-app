43e530cc04dacf20382140a9bb66f1fd5a425c1f
sjd58 <dicksonsj0798@gmail.com>
2022-01-05 15:14:59 -0500

    Wrapped Pokemon List Array in IIFE, Included ForEach loop
    Task submission for 1.5, first version

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,66 @@
 //PokemonList array featuring Legendary Bird Pokemon from Generations 1 and 2:
-let pokemonList = [
-    {
-        name: 'Articuno',
-        type: ['flying', 'ice'],
-        height: 9,
-        pokedexNumber: '#145',
-    },
-    {
-        name: 'Zapdos',
-        type: ['flying', 'electric'],
-        height: 10,
-        pokedexNumber: '#145',
-    },
-    {
-        name: 'Moltres',
-        type: ['flying', 'fire'],
-        height: 12,
-        pokedexNumber: '#146',
-    },
-    {
-        name: 'Lugia',
-        type: ['flying', 'psychic'],
-        height: 14,
-        pokedexNumber: '#249',
-    },
-    {
-        name: 'Ho-oh',
-        type: ['flying', 'fire'],
-        height: 13,
-        pokedexNumber: '#250',
-    },
-];
-//Loop to list Pokemon facts
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' ' + 'height: ' + pokemonList[i].height);
-    //Conditional to insert a comment about Pokemon size
-    if(pokemonList[i].height > 12) {
-        document.write(' Wow! That\'s a big Pokemon!');
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Articuno',
+            type: ['flying', 'ice'],
+            height: 9,
+            pokedexNumber: '#145',
+        },
+        {
+            name: 'Zapdos',
+            type: ['flying', 'electric'],
+            height: 10,
+            pokedexNumber: '#145',
+        },
+        {
+            name: 'Moltres',
+            type: ['flying', 'fire'],
+            height: 12,
+            pokedexNumber: '#146',
+        },
+        {
+            name: 'Lugia',
+            type: ['flying', 'psychic'],
+            height: 14,
+            pokedexNumber: '#249',
+        },
+        {
+            name: 'Ho-oh',
+            type: ['flying', 'fire'],
+            height: 13,
+            pokedexNumber: '#250',
+        },
+    ];
+
+    //Public Functions
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-    document.write('<br>');
-}
+    
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+}());
+
+//Using a public function to add a legendary Pokemon to the list:
+pokemonRepository.add({
+    name: 'Kyogre',
+    type: 'water',
+    height: 15,
+    pokedexNumber: '#382'
+})
+
+//Loop to list Pokemon facts
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height >= 14) {
+        document.write('<p>' + pokemon.name + ' (' + 'height: ' + pokemon.height + ') ' + pokemon.pokedexNumber + ' Wow! That\'s a big Pokemon!' + '</p>')
+    } else {
+    document.write('<p>' + pokemon.name + ' (' + 'height: ' + pokemon.height + ') ' + pokemon.pokedexNumber + '</p>')
+    }
+});
