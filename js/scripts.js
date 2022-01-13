b7d3526ffeeb0237bc3cf72e161ee6f734f7ed8d
sjd58 <dicksonsj0798@gmail.com>
2022-01-12 22:37:53 -0500

    Changing js file to load pokemon data from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,107 @@
-//repository is an array featuring Legendary Pokemon
+
 let pokemonRepository = (function () {
-    let repository = [
-        {
-            name: 'Articuno',
-            type: ['flying', 'ice'],
-            height: 9,
-            pokedexNumber: '#145',
-        },
-        {
-            name: 'Zapdos',
-            type: ['flying', 'electric'],
-            height: 10,
-            pokedexNumber: '#145',
-        },
-        {
-            name: 'Moltres',
-            type: ['flying', 'fire'],
-            height: 12,
-            pokedexNumber: '#146',
-        },
-        {
-            name: 'Lugia',
-            type: ['flying', 'psychic'],
-            height: 14,
-            pokedexNumber: '#249',
-        },
-        {
-            name: 'Ho-oh',
-            type: ['flying', 'fire'],
-            height: 13,
-            pokedexNumber: '#250',
-        },
-    ];
+
+
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
 
     //Public Functions
     function add(pokemon) {
         if (
             typeof pokemon === 'object' &&
-            'name' in pokemon &&
-            'type' in pokemon &&
-            'height' in pokemon &&
-            'pokedexNumber' in pokemon
+            'name' in pokemon
         ) {
-        repository.push(pokemon);
+        pokemonList.push(pokemon);
         } else {
-            console.log('new pokemon information is not correct');
+            console.log('New Pokemon information is not correct');
         }
     }
+
+
     function getAll() {
-        return repository;
+        return pokemonList;
     }
+
+
     function addListItem(pokemon) {
         let pokemonList = document.querySelector('.pokemon-list');
         let listPokemon = document.createElement('li');
         let button = document.createElement('button');
 
+        addListItem.classList.add('listItem-class');
+
         button.innerText = pokemon.name;
         button.classList.add('button-class');
 
-        listPokemon.appendChild(button);
-        pokemonList.appendChild(listPokemon);
+        listPokemon.item.appendChild(button);
+    }
 
-        //click event, display clicked pokemon name in the console
-        button.addEventListener('click', function() {
-            console.log(pokemon)
+
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // Now we add the details to the item
+            item.imageUrl = details.sprites.font_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
         });
     }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
+    }    
+
     return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        loadList: loadList,
+        addListItem: addListItem,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     };
 }());
 
-//Using a public function to add a legendary Pokemon to the list:
-pokemonRepository.add({
-    name: 'Kyogre',
-    type: 'water',
-    height: 15,
-    pokedexNumber: '#382'
-});
-
 console.log(pokemonRepository.getAll());
 
+pokemonRepository.loadList().then(function() {
+    //Now the data is loaded!
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
+
+function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+        console.log(pokemon);
+    });
+}
+
 //Loop to list Pokemon facts
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
